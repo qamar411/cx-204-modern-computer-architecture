49267345f46674b4a6408f4c8e96e47bd1733e5a
Qamar Moavia <qmoavia.bee19seecs@seecs.edu.pk>
2024-12-16 10:15:42 +0300

    added lab1 task2

---
+++ labs/lab01/task2/hello.rv32.asm
@@ -0,0 +1,43 @@
+	.file	"hello.c"
+	.option nopic
+	.attribute arch, "rv32i2p1"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.string	"<Your-Name>"
+	.text
+	.align	2
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	addi	sp,sp,-16
+	.cfi_def_cfa_offset 16
+	sw	ra,12(sp)
+	sw	s0,8(sp)
+	.cfi_offset 1, -4
+	.cfi_offset 8, -8
+	addi	s0,sp,16
+	.cfi_def_cfa 8, 0
+	lui	a5,%hi(.LC0)
+	addi	a0,a5,%lo(.LC0)
+	call	printf
+	li	a5,0
+	mv	a0,a5
+	lw	ra,12(sp)
+	.cfi_restore 1
+	lw	s0,8(sp)
+	.cfi_restore 8
+	.cfi_def_cfa 2, 16
+	addi	sp,sp,16
+	.cfi_def_cfa_offset 0
+	jr	ra
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: () 14.2.0"
+	.section	.note.GNU-stack,"",@progbits
